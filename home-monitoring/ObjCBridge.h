8f221f1692489b3ec5b6a99b79c5bcbaf644a711
Konrad <konrad.sopala@gmail.com>
2017-06-22 22:47:05 +0200

    Integrated EstimoteiOSSDK.

---
+++ home-monitoring/ObjCBridge.h
@@ -0,0 +1,14 @@
+//
+//  ObjCBridge.h
+//  home-monitoring
+//
+//  Created by Konrad on 22.06.2017.
+//  Copyright © 2017 Konrad. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+#import <EstimoteSDK/EstimoteSDK.h>
+
+#endif /* ObjCBridge_h */
