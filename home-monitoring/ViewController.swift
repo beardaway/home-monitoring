7c167f57d972c51d3458072ac157ed51948f51a8
Konrad <konrad.sopala@gmail.com>
2017-06-22 23:03:31 +0200

    Temperature sensor added.

--- home-monitoring/ViewController.swift
+++ home-monitoring/ViewController.swift
@@ -8,16 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ESTDeviceManagerDelegate, ESTDeviceConnectableDelegate{
+    
+    
+    @IBOutlet weak var temperatureInfoLabel: UILabel!
+    
+    var monitoringDevice: ESTDeviceLocationBeacon?
+    let monitoringDeviceIdentifier: String = "4e4fdfa0dc89ddde6397211621338628"
+    lazy var monitoringDeviceManager: ESTDeviceManager = {
+        let manager = ESTDeviceManager()
+        manager.delegate = self
+        return manager
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let beaconFilter = ESTDeviceFilterLocationBeacon(identifier: self.monitoringDeviceIdentifier)
+        self.monitoringDeviceManager.startDeviceDiscovery(with: beaconFilter)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func deviceManager(_ manager: ESTDeviceManager, didDiscover devices: [ESTDevice]) {
+        guard let device = devices.first as? ESTDeviceLocationBeacon else { return }
+        self.monitoringDeviceManager.stopDeviceDiscovery()
+        self.monitoringDevice = device
+        self.monitoringDevice?.delegate = self
+        self.monitoringDevice?.connect()
+    }
+    
+    func estDeviceConnectionDidSucceed(_ device: ESTDeviceConnectable) {
+        print("Connection Output Status: Connected")
+        let temperatureInfo = Int((monitoringDevice?.settings?.sensors.temperature.getValue())!)
+        temperatureInfoLabel.text = "\(temperatureInfo) °C"
+    }
+    
+    func estDevice(_ device: ESTDeviceConnectable, didFailConnectionWithError error: Error) {
+        print("Connection Output Status: \(error.localizedDescription)")
+    }
+    
+    func estDevice(_ device: ESTDeviceConnectable, didDisconnectWithError error: Error?) {
+        print("Connection Output Status: Disconnected")
     }
 
 
